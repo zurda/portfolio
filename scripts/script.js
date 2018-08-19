5554da5ea43d537a2e8750ffbf639c43635cdd0f
zurda <m.weizman@gmail.com>
2018-08-19 15:13:58 +0100

    Added info text

--- scripts/script.js
+++ scripts/script.js
@@ -104,3 +104,20 @@ if ('querySelector' in document &&
         });
     });
 }
+
+
+function readMore() {
+  var dots = document.getElementById("line-breaks");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("read-more-btn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
